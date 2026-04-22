ed780de9526b9832a1dec276e0d6835c9b062fc6
IT24104235 <IT24104235@my.sliit.lk>
2026-04-22 13:21:03 +0530

    update gitignore to ignore all node_modules

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  email: String
 });
 
 module.exports = mongoose.model("User", UserSchema);
